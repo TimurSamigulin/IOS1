c546ef16760a32f7e1d5f0567a1de81d01fd545b
denisturygin <etosurr@gmail.com>
2020-04-19 22:34:18 +0700

    Initial Commit

---
+++ SpaceInvaders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceInvaders
+//
+//  Created by Денис Турыгин on 19.04.2020.
+//  Copyright © 2020 Timur Samigulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
